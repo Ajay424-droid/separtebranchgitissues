a7bef3d3991b82d7883705fb6ee25a5b756d1770
Ajay Patel <ap1892503@gmail.com>
2025-12-26 12:50:17 +0530

    Initialize a simple Flask applicationapp.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask App!"
+
+@app.route("/about")
+def about():
+    return "This is a simple Flask application."
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
