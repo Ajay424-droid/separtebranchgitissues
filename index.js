3d50c8d40d68a05bd15c6f7855ce61772b4d5c6b
Ajay Patel <ap1892503@gmail.com>
2025-12-26 10:49:39 +0530

    index.js

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
-from flask import Flask, request, jsonify
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
-app = Flask(__name__)
+const app = express();
+dotenv.config();
 
-# Home API (GET)
-@app.route("/", methods=["GET"])
-def home():
-    return "Hello, this is Python Backend!"
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("common"));
 
-# About API (GET)
-@app.route("/about", methods=["GET"])
-def about():
-    return jsonify({
-        "project": "Simple Flask Backend",
-        "status": "Running"
-    })
+// mysql connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: "TEST",
+  port: process.env.DB_PORT
+});
 
-# Data API (POST)
-@app.route("/data", methods=["POST"])
-def get_data():
-    data = request.json
-    name = data.get("Ajay")
-    return jsonify({
-        "message": f"Hello {name}, data received successfully"
-    })
+// connect database
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Database connected successfully");
+  }
+});
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+// test route
+app.get("/", (req, res) => {
+  res.json("Server is running");
+});
+
+// sample api (fetch data)
+app.get("/users", (req, res) => {
+  const sql = "SELECT * FROM users";
+  db.query(sql, (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result);
+  });
+});
+
+// server start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
