d8eeaa68161c1614d0b118686ae9b14a7a32b64c
Ajay Patel <ap1892503@gmail.com>
2025-12-26 11:41:51 +0530

    update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get("/users", (req, res) => {
 });
 
 // server start
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
