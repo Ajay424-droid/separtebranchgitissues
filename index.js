ea3dae6fe1e39f83c11ec7a00cf00b9a98aeacd6
Ajay <ap1892503@gmail.com>
2025-12-26 12:11:31 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "test";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("common"));
+
+// mysql connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: "TEST",
+  port: process.env.DB_PORT
+});
+
+// connect database
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Database connected successfully");
+  }
+});
+
+// test route
+app.get("/", (req, res) => {
+  res.json("Server is running");
+});
+
+// sample api (fetch data)
+app.get("/users", (req, res) => {
+  const sql = "SELECT * FROM users";
+  db.query(sql, (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result);
+  });
+});
+
+// server start
+const PORT = process.env.PORT || 5002222222222222222222222;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
